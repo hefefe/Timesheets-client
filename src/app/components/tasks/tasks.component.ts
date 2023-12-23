85f48e2c5eb803c267475d3a23879b42f646252a
Michał Witczak <michal.witczak666@gmail.com>
2023-12-23 23:53:54 +0100

    tasks c.d.

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -40,4 +40,6 @@ export class TasksComponent {
 
     return `${date[2]<10?'0'+date[2]:date[2]}-${date[1]<10?'0'+date[1]:date[1]}-${date[0]} ${date[3]}:${date[4]}:${date[5]}`;
   }
+
+  openNew(){}
 }
